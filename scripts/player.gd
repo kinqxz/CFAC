c1d12f9de89c2032b8083029c8612b606f20dc57
kinqxz <jamesericgamertv@gmail.com>
2025-03-05 23:29:22 -0500

    Movement should be good now

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var player: CharacterBody2D = $"."
 @onready var obstaclesNode: Node = $"../Obstacles"
 
@@ -13,19 +12,19 @@ var obstacleFound : AnimatedSprite2D = null
 #const JUMP_VELOCITY = -400.0
 #var health = 100.0
 
-func move(to: Vector2) -> void:
-	var animation = $AnimatedSprite2D/AnimationPlayer.get_animation("move")
-	var track = animation.find_track("AnimatedSprite2D:position", Animation.TYPE_VALUE)
-	var first_key = animation.track_get_key_count(track) - 2
-	var last_key = animation.track_get_key_count(track) - 1
-	
-	var x = -(player.position.x - to.x)
-	var y = -(player.position.y - to.y)
-	
-	animation.track_set_key_value(track, first_key, Vector2.ZERO)
-	animation.track_set_key_value(track, last_key, Vector2(x,y))
-	$AnimatedSprite2D/AnimationPlayer.play("move")
-	player.position = to
+#func move(to: Vector2) -> void:
+	#var animation = $AnimatedSprite2D/AnimationPlayer.get_animation("move")
+	#var track = animation.find_track("AnimatedSprite2D:position", Animation.TYPE_VALUE)
+	#var first_key = animation.track_get_key_count(track) - 2
+	#var last_key = animation.track_get_key_count(track) - 1
+	#
+	#var x = -(player.position.x - to.x)
+	#var y = -(player.position.y - to.y)
+	#
+	#animation.track_set_key_value(track, first_key, Vector2.ZERO)
+	#animation.track_set_key_value(track, last_key, Vector2(x,y))
+	#$AnimatedSprite2D/AnimationPlayer.play("move")
+	#player.position = to
 	
 func findObstacle(playerPosition : Vector2, direction: Vector2) -> AnimatedSprite2D:
 	#for number in range(1, 15):
@@ -56,11 +55,14 @@ func findObstacle(playerPosition : Vector2, direction: Vector2) -> AnimatedSprit
 									if deltaY <= jumpingDistance && deltaY > 0:
 										lastObstacle = icon
 						elif direction == Vector2.DOWN:
+							print("why")
 							if currentDeltaY < deltaY:
+								print("hii")
 								deltaX = currentDeltaX
 								deltaY = currentDeltaY
 								
 								if deltaX == 0:
+									print("hel")
 									if deltaY >= (-1 * jumpingDistance) && deltaY < 0:
 										lastObstacle = icon
 						elif direction == Vector2.LEFT:
@@ -79,8 +81,25 @@ func findObstacle(playerPosition : Vector2, direction: Vector2) -> AnimatedSprit
 								if deltaX <= jumpingDistance && deltaX > 0:
 									if deltaY == 0:
 										lastObstacle = icon
-				
+	
 	return lastObstacle
+
+func checkPlayerRotation(direction : Vector2) -> void:
+	if direction == Vector2.DOWN:
+		if player.rotation_degrees != 0.0:
+			player.rotation_degrees = 0.0
+	elif direction == Vector2.UP:
+		if player.rotation_degrees != 180.0:
+			player.rotation_degrees = 180.0
+	elif direction == Vector2.LEFT:
+		if player.rotation_degrees != 270.0:
+			player.rotation_degrees = 270.0
+	elif direction == Vector2.RIGHT :
+		if player.rotation_degrees != 90.0:
+			player.rotation_degrees = 90.0
+
+func _ready() -> void:
+	player.position = initPosition
 
 func _physics_process(delta):
 	var direction = Input.get_vector("look_left", "look_right", "look_down", "look_up")
@@ -94,22 +113,21 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_released("initial_position"):
 		player.position = initPosition
-		animated_sprite_2d.play("jump_right")
 	
 	if Input.is_action_just_released("confirm"):
 		if obstacleFound != null:
-			move(obstacleFound.position)
-			
 			if lastObstacleFound != null:
 				lastObstacleFound.emit_signal("submerge")
 			
+			obstacleFound.emit_signal("chosen_stop")
 			lastObstacleFound = obstacleFound
+			player.position = obstacleFound.position
 			obstacleFound = null
 			
 	if direction != Vector2.ZERO:
 		if lastDirection == Vector2.DOWN:
 			#This is actually up its just that the vectors are confusing
-			animated_sprite_2d.play("look_up")
+			checkPlayerRotation(Vector2.DOWN)
 			var obstacle = findObstacle(player.get_position_delta(), Vector2.DOWN)
 			
 			if obstacle != null:
@@ -118,7 +136,7 @@ func _physics_process(delta):
 				obstacle.emit_signal("chosen")
 				obstacleFound = obstacle
 		if lastDirection == Vector2.RIGHT:
-			animated_sprite_2d.play("look_right")
+			checkPlayerRotation(Vector2.RIGHT)
 			var obstacle = findObstacle(player.get_position_delta(), Vector2.RIGHT)
 				
 			if obstacle != null:
@@ -127,7 +145,7 @@ func _physics_process(delta):
 				lastDirection = Vector2.ZERO
 				obstacleFound = obstacle
 		if lastDirection == Vector2.LEFT:
-			animated_sprite_2d.play("look_left")
+			checkPlayerRotation(Vector2.LEFT)
 			var obstacle = findObstacle(player.get_position_delta(), Vector2.LEFT)
 				
 			if obstacle != null:
@@ -137,7 +155,7 @@ func _physics_process(delta):
 				obstacleFound = obstacle
 		if lastDirection == Vector2.UP:
 			#This is actually down
-			animated_sprite_2d.play("look_down")
+			checkPlayerRotation(Vector2.UP)
 			var obstacle = findObstacle(player.get_position_delta(), Vector2.UP)
 			
 			if obstacle != null:
